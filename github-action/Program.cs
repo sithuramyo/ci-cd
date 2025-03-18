1b9995ea215d280ff290f438d8de238826ccf644
Sithura Myo <66086921+sithuramyo@users.noreply.github.com>
2025-03-18 11:05:23 +0630

    Fixed

--- github-action/Program.cs
+++ github-action/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
-
+var url = "http://0.0.0.0:80";
+builder.WebHost.UseUrls(url);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
